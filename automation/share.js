2b2b490e0290e4c9e717259070639927a67b5ea3
Gavin Palmer <gavinpalmer1984@gmail.com>
2019-07-07 21:50:48 -0500

    handle ads

--- automation/share.js
+++ automation/share.js
@@ -1,9 +1,31 @@
 const assert = require('assert');
 
-describe('webdriver.io page', () => {
+describe('channel', () => {
     it('should have the right title', () => {
-        browser.url('https://webdriver.io');
-        const title = browser.getTitle();
-        assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+        browser.url('https://www.youtube.com/channel/UC6JhS4GvWf3AJfOTfkrse2w/videos');
+        assert.equal('Simulation', $('#channel-title').getText());
+    });
+
+    it('should click latest video', () => {
+        $('.ytd-thumbnail').waitForDisplayed();
+        $$('.ytd-thumbnail')[0].click();
+    });
+
+    it('should skip ads', () => {
+        $('#player').waitForDisplayed();
+        console.log('gp1', new Date());
+        browser.pause(2000);
+        while ($$('.ad-showing').length) {
+            console.log('gp2', new Date());
+            browser.pause(1000);
+        }
+        console.log('gp3', new Date());
+    });
+
+    it('should click share', () => {
+        browser.debug();
+        // $$('.ytp-ad-skip-button-slot')[0].click(); // skip ad
+        $('button[aria-label="Share"]').waitForDisplayed();
+        $('button[aria-label="Share"]').click();
     });
 });
